0354023b5af9c3b91648ca77993a726d6d7e855c
Jessie Guo <yangzigu@usc.edu>
2015-12-31 16:05:35 -0800

    code files

---
+++ Scripts/Shield.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+
+public float toughness = 1.0f;
+	float health; 
+
+	// Use this for initialization
+	void Start () {
+		health = toughness;
+	}
+	
+	// Update is called once per frame
+	void OnCollisionEnter2D(Collision2D coll) {
+	Destroy(coll.gameObject);
+
+	health -= 1.0f;
+	
+	}
+	
+		}
+
+	
---
+++ Scripts/UFOScore.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UFOScore : MonoBehaviour {
+
+	int pointValue = 200;
+	
+	GameObject scoreObject;
+	ScoreDisplay scoreScript;
+	
+	//	bool selfDestruct = false;
+	
+	void Start () {	
+		scoreObject = GameObject.Find ("Score");
+		scoreScript = scoreObject.GetComponent<ScoreDisplay>();	
+	}
+	
+	void Update () {
+		//		if (selfDestruct) {
+		//			Destroy (gameObject);
+		//		}
+	}
+	
+	void OnTriggerEnter2D (Collider2D otherCollider) {
+		if (otherCollider.gameObject.tag == "PlayerMissile") {
+			scoreScript.UpdateScore (pointValue);
+		}
+	}
+}
